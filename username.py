38d19a5ec844f5aa23dbfb6e6d0088876dccfcee
danielk-JetLearner <charredaji@gmail.com>
2026-04-27 15:56:19 -0700

    Add files via upload

--- username.py
+++ username.py
@@ -1,5 +1,9 @@
 while True:
     user=input ("what is your full name?")
+    print(user.split())
+    name=user.split()
     if user.lower()=='exit':
         break
     
+    if len(name)<2:
+        continue
