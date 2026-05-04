3f8c892d99dbc16986d09e0058b21cb1fc723e6c
danielk-JetLearner <charredaji@gmail.com>
2026-05-04 15:50:30 -0700

    Add files via upload

--- username.py
+++ username.py
@@ -7,3 +7,12 @@ while True:
     
     if len(name)<2:
         continue
+    name[0]
+    first = name[0]
+    last = name[1]
+    first.lower()
+    last.lower()
+    print(first)
+    print(last)
+    daniel = first[0:4] + last[0:3] + "35"
+    print(daniel)
